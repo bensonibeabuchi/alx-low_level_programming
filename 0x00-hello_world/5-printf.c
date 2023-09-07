48f5a0855596af09485c11dd1112581090ca9e5e
Benson Ibeabuchi <ibeabuchibenson@gmail.com>
2023-09-07 12:11:23 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -it prints exactly the input
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
