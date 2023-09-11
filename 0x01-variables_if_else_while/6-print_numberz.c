8470431a5b3cec464229dab7af4eb6d8ff78d5f1
Benson Ibeabuchi <ibeabuchibenson@gmail.com>
2023-09-11 03:47:25 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,16 @@
 
 /**
  * main - Entry point
- * Retun: Always 0 (success)
+ * * Return: Always 0 (sucess)
  */
 
 int main(void)
-
 {
-	int i;
+	int d;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
 
 	return (0);
 }
