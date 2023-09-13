99e079eb4cd2044e30da64f3a8724ee27421f742
Benson Ibeabuchi <ibeabuchibenson@gmail.com>
2023-09-13 04:56:34 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,22 @@ void times_table(void)
 {
 	int n, j, k, l, m;
 
-	for (n = 0;n < 10;n++)
+	for (n = 0;n  < 10; n++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			k = n * j;
-			if(k < 10 && j != 0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(44);
 				_putchar(32);
 				_putchar(32);
 			}
-			if(k < 10)
+			if (k < 10)
 				_putchar('0' + k);
 
 
-			if(k > 9)
+			if (k > 9)
 			{
 				l = k / 10;
 				m = k % 10;
