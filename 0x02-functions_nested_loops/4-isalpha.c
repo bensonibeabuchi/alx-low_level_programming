57ec7450a871cb202b45ac5873c0f27dba72d508
Benson Ibeabuchi <ibeabuchibenson@gmail.com>
2023-09-13 04:16:26 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code.
- * Return: Always 0.
+ * _isalpha - Check for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alpahbetic character or 0 for anything else
  */
 
-int main(void)
+int _isalpha(int c)
 {
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(100);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
 	return (0);
 }
